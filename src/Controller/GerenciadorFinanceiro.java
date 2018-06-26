4d89600774050684874cad55f69ff93a18bcbf34
SantiagoSaraiva <39315884+SantiagoSaraiva@users.noreply.github.com>
2018-06-26 17:07:48 -0300

    Painel Financeiro

--- src/Controller/GerenciadorFinanceiro.java
+++ src/Controller/GerenciadorFinanceiro.java
@@ -5,6 +5,15 @@ import java.util.ArrayList;
 
 public class GerenciadorFinanceiro {
     
+    public static double custoJaulas(ArrayList<Jaula> jaulas){
+        double custo = 0;
+        
+        for(Jaula j : jaulas){
+            custo = custo + j.custoTotal();
+        }
+        return custo;
+    }
+    
     public static double custoFuncionarios(ArrayList<Funcionario> funcionarios){
         double custo = 0;
         
@@ -14,12 +23,30 @@ public class GerenciadorFinanceiro {
         return custo;
     }
     
-    public static double custoJaulas(ArrayList<Jaula> jaulas){
+    public static double custoAnimais()
+    {
         double custo = 0;
         
-        for(Jaula j : jaulas){
-            custo = custo + j.custoTotal();
-        }
+        
+        
         return custo;
+    }
+    
+    public static double ganhoLojas()
+    {
+        double ganho = 0;
+        
+        
+        
+        return ganho;
+    }
+    
+    public static double ganhoVisitantes()
+    {
+        double ganho = 0;
+        
+        
+        
+        return ganho;
     }
 }
